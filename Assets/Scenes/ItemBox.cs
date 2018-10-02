c1e486eac39ef2008b87b360b76469d7e08a61c4
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2018-10-02 12:17:23 +0900

    Update ItemBox.cs

--- Assets/Scenes/ItemBox.cs
+++ Assets/Scenes/ItemBox.cs
@@ -13,18 +13,22 @@ public class ItemBox : MonoBehaviour {
     private Color originalColor;
 
 	// Use this for initialization
+	// 初期化のためにこれを使用
 	void Start () {
         myRenderer = GetComponent<Renderer>();
         originalColor = myRenderer.material.color;
 	}
 	
 	// Update is called once per frame
+	// アップデートはフレームごとに一度ずつ呼び出し
 	void Update () {
 		
 	}
     
     // 트리거인 콜라이더와 충돌할때 자동으로 실행
+    // トリガーのコライダー来て衝突するときに自動に実行
     // Enter 충돌한 그 순간
+    // エンター 衝突したその瞬間
     void OnTriggerEnter(Collider other)
     {
 
@@ -34,11 +38,12 @@ public class ItemBox : MonoBehaviour {
             myRenderer.material.color = touchColor;
           
         }
-        Debug.Log("충돌 했음!");
+        Debug.Log("충돌 했음! //衝突した!");
 
     }
 
     //Exit 붙어있다가 떼어질때
+    //Exit あって、外されとき
     void OnTriggerExit(Collider other)
     {
         if (other.tag == "EndPoint")
@@ -50,6 +55,7 @@ public class ItemBox : MonoBehaviour {
     }
 
     // Stay 충돌하고 있는 혹은 붙어 있는'동안'
+    // Stay 衝突しているあるいはついている間	
 
     void OnTriggerStay(Collider other)
     {
@@ -62,8 +68,9 @@ public class ItemBox : MonoBehaviour {
     }
 
     // 일반 콜라이더와 충돌했을때 자동으로 실행
+    //一般コライダー来て衝突したときに自動に実行
    /* void OnCollisionEnter(Collision other)
     {
-        Debug.Log("충돌 했음!");
+        Debug.Log("충돌 했음!//衝突した!");
     }*/
 }
